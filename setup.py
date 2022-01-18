62835a849995507f8bcb394c3e2e95248be58303
Jia-Xin Zhu <xmuchiahsin@gmail.com>
2022-01-18 20:49:04 +0800

    update

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 """
-Setup script for `zjxpack`
+Setup script for WatDynamics
 """
 
 from setuptools import setup, find_packages
 
 setup(
-    name = "zjxpack",
-    version = "0.1",
+    name="watdyn",
+    version="0.0",
     # include all packages in src
-    packages = find_packages(),  
-    # exclude by
-    #find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
-    # all packages required for this package is in src
-    #package_dir = {'':'zjxpack'},   
-    include_package_data = True,
-    # exclude all update.sh
-    exclude_package_data = { '': ['update.sh'] }
-)
-
+    packages=find_packages(),
+    include_package_data=True)
