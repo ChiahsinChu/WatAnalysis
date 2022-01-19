123c0aa35a4eacbc0608c1c56e3287b39b87934e
Jia-Xin Zhu <xmuchiahsin@gmail.com>
2022-01-19 10:46:15 +0800

    add test for PartialHBAnalysis

---
+++ tests/PHBA/serial.py
@@ -0,0 +1,28 @@
+import numpy as np
+import time
+import MDAnalysis as mda
+from MDAnalysis import transformations as trans
+
+from watdyn.m_mda import PartialHBAnalysis
+
+# load trajectory
+u = mda.Universe("../input_data/interface.psf", "../input_data/trajectory.xyz")
+dim = [16.869, 16.869, 41.478, 90, 90, 120]
+transform = trans.boxdimensions.set_dimensions(dim)
+u.trajectory.add_transformations(transform)
+
+start = time.time()
+hbonds = PartialHBAnalysis(universe=u,
+                           region=[2.7, 4.5],
+                           surf_ids=[np.arange(871, 906),
+                                     np.arange(691, 726)],
+                           donors_sel=None,
+                           hydrogens_sel="name H",
+                           acceptors_sel="name O",
+                           d_a_cutoff=3.0,
+                           d_h_a_angle_cutoff=150,
+                           update_selections=False)
+hbonds.run()
+print(hbonds.results.hbonds.shape)
+fmt = "\nWork Completed! Used Time: {:.3f} seconds"
+print(fmt.format(time.time() - start))
